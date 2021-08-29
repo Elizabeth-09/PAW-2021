c1e1a07ae99d294e173f809f72d822c378cce29e
Elizabeth-09 <>
2021-08-29 09:43:59 -0600

    update

--- views/admin/index.php
+++ views/admin/index.php
@@ -12,8 +12,22 @@ $redic->Admin();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrador</title>
+      <!--CSS-->
+      <link rel="stylesheet" href="./public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./public/css/boststrap-theme.css">
+    <link rel="stylesheet" href="./public/css/estilo.css">
+    <!--JS-->
+    <script src="./public/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="./public/js/jquery-1.9.1.min.js"></script>
+    <script src="./public/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/05f4903dc9.js" crossorigin="anonymous"></script>
 </head>
 <body>
+    <?php include 'navbar/navbar.php';?>
+    <div class="container-fluid">
+        <div class="alert alert-success" style="width: 400px;">
+        </div>
+    </div>
     <h3><b>Bienvenido/a <?php echo $user; ?></b></h3>
 </body>
 </html>
